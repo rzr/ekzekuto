1ad1ecaa34f59cb790ee26c2638b34b3c7b64b4e
Philippe Coval <philippe.coval@osg.samsung.com>
2016-03-10 15:25:57 +0100

    logic: add: src/main.h (import)
    Change-Id: I7f9cef660f8c7269294960280a11c4db14bbddd7
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ src/main.h
@@ -0,0 +1,19 @@
+#ifndef __main_H__
+#define __main_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "ekzekuto"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.ekzekuto"
+#endif
+
+#endif /* __main_H__ */
