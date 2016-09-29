19021b663586504592b1ce1eb99d17db97a9e479
Philippe Coval <philippe.coval@osg.samsung.com>
2016-09-29 14:38:30 +0200

    script: Look for developer or user script
    demo uses pipes
    
    Change-Id: Ibfb8b6f00fd92ca0deeef3a1508fd105a4c8d0ab
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- lib/runme.sh
+++ lib/runme.sh
@@ -1,23 +1,41 @@
 #!/bin/sh
-set -x
 
 log_()
 {
     echo "log: $@"
-    $@
+    "$@"
     echo ""
+}
+
+demo_()
+{
+    set -x
+    log_ date -u
+    log_ cat /etc/os-release
+    log_ cat /proc/version
+    log_ /sbin/ifconfig -a | grep addr
+    log_ cat /proc/uptime
+    log_ df -h
+    log_ date
 }
 
 main_()
 {
-    echo "# ${PKG_NAME}: { starting"
-    log_ date
-    log_ cat /proc/version
-    log_ cat /proc/uptime
-    log_ df -h
-    log_ /sbin/ifconfig
-    echo "# ${PKG_NAME}: } stopping"
+    echo "#{ starting: ${PKG_NAME}"
+    dir="/home/developer/apps/${PKG_NAME}/"
+    file="${dir}/runme.sh"
+    if [ -x "$file" ] ; then
+        log_ "$file"
+    else 
+        dir="/opt/usr/media/Others/apps/${PKG_NAME}/"
+        file="${dir}/runme.sh"
+        if [ -x "$file" ] ; then
+            log_ "$file"
+        else
+            demo_ "$@"
+        fi
+    fi
+    echo "#} stopping: ${PKG_NAME}"
 }
 
-main_
-
+main_ "$@"
